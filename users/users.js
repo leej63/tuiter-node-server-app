4c024f47f219bea4f024f5c0ac2de82e1ccf1342
Jonathan Lee <jonathanlee@Jonathans-MacBook-Air.local>
2023-08-08 22:14:45 -0700

    a5 update

--- users/users.js
+++ users/users.js
@@ -1,5 +1,5 @@
 export default [
-    { "username": "alice",   "type": "FACULTY", "_id": "123" },
-    { "username": "bob",     "type": "STUDENT", "_id": "234" },
-    { "username": "charlie", "type": "FACULTY", "_id": "345" }
+    { "username": "alice",   "type": "FACULTY", "_id": "123", "firstName": "Alice", "lastName": "Cao" },
+    { "username": "bob",     "type": "STUDENT", "_id": "234", "firstName": "Bob", "lastName": "Smith" },
+    { "username": "charlie", "type": "FACULTY", "_id": "345", "firstName": "Charlie", "lastName": "Fu" }
 ]  
