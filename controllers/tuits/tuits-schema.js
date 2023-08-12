b50fc623d544f0b1146128bc064490c0c2e45567
Jonathan Lee <jonathanlee@Jonathans-MacBook-Air.local>
2023-08-11 23:30:59 -0700

    a6 update to tuits schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -2,8 +2,18 @@ import mongoose from 'mongoose';
 
 
 const schema = mongoose.Schema({
-    tuit: String,
-    likes: Number,
+    topic: String,
+    userName: String,
+    handle: String,
+    time: String,
+    image: String,
+    title: String,
     liked: Boolean,
+    likes: Number,
+    disliked: Boolean,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number,
+    tuit: String,
 }, {collection: 'tuits'});
 export default schema;
